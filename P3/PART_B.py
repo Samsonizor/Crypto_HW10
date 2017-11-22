8683ba85de332f567769a9e7e476a64638b5c6f4
Samsonizor <samsonizor@gmail.com>
2017-11-22 14:27:50 -0500

    Added z values given in problem Began to define s-box function

--- P3/PART_B.py
+++ P3/PART_B.py
@@ -1,5 +1,12 @@
 from numpy import polydiv
 
+def sbox_AES(input):
+    """
+    perform the AES s-box on the input
+    :param input: s-box input
+    :return: result of the s-box
+    """
+
 # AES Values
 AES_S_BOX = [[1, 0, 0, 0, 1, 1, 1, 1],
              [1, 1, 0, 0, 0, 1, 1, 1],
@@ -11,4 +18,11 @@ AES_S_BOX = [[1, 0, 0, 0, 1, 1, 1, 1],
              [0, 0, 0, 1, 1, 1, 1, 1]]
 
 REDUCING_POLY = [1,0,0,0,1,1,0,1,1]
+
+# Values given in problem 3b
+Z = [[None],
+     [0,0,0,0,0,0,0,0],
+     [0,0,0,0,0,0,0,1],
+     [0,0,0,0,0,0,1,0],
+     [0,0,0,0,0,0,1,1]]
 
